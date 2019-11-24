3918ad068aa6e8d0c491715d415147124b9c849c
wsdmzy <949328973@qq.com>
2019-11-24 19:15:00 +0800

    wx

--- leetcode/min_stack/main.js
+++ leetcode/min_stack/main.js
@@ -6,28 +6,49 @@ function MinStack() {
   this.min = Number.MAX_VALUE;
 }
 MinStack.prototype.push = function(x) {
-  if (x < this.min) {
-    this.min = x;
-  }
+  // if (x < this.min) {
+  //   this.min = x;
+  // }
+    this.min = Math.min(this.min,x);
   return this.stack.push(x);
 }
 MinStack.prototype.pop = function() {
-  const item = this.stack.pop();
-  return item;
+  // let i = this.stack.pop();
+  // if (i === this.min) {
+  //   this.min = this.stack[0];
+  //   for (var j = 1; j < this.stack.length; j += 1) {
+  //     if(this.min > this.stack[j]) {
+  //       this.min = this.stack[j];
+  //     }
+  //   }
+  // }
+  if(!this.stack.length)
+    return 0
+    let s = this.stack.pop()
+    if(this.min === s){
+    console.log(...this.stack);
+    this.min = Math.min(...this.stack);  //所有的元素
+    }
 }
 MinStack.prototype.top = function() {
-  return this.stack[this.stack.length - 1];
+  let len = this.stack.length
+  if (!len)
+    return 0;
+  return this.stack[len - 1];
 }
 MinStack.prototype.getMin = function() {
   return this.min;
 }
 MinStack.prototype.toString = function() {
   return this.stack.join(',');
+  // return ...this.stack;
 }
 var stack = new MinStack();
 stack.push(-2);
 stack.push(0);
 stack.push(-3);
-stack.push(-9);
-console.log(stack.toString())
+console.log(stack.getMin());
+stack.pop();
+console.log(stack.toString());
+console.log(stack.top());
 console.log(stack.getMin());
