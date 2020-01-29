234d54948b18dca9a86c5172ce8739a89914f92b
wsdmzy <949328973@qq.com>
2020-01-29 21:22:19 +0800

    组件化开发

---
+++ vueLearn/03-Vue/es6-modules/aa.js
@@ -0,0 +1,9 @@
+let flag = true;
+
+export {
+  flag
+}
+
+// export var a = 1; 导出一个
+
+// export default 默认
---
+++ vueLearn/03-Vue/es6-modules/bb.js
@@ -0,0 +1,9 @@
+import {flag} from './aa.js'
+// 全部导入
+// import * as aa from './aa.js'
+
+console.log(flag)
+if (flag) {
+  console.log('成功')
+}
+console.log('-----')
